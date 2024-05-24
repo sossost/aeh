0c9e1f1f9b96eb51d794bc708f11ea7b89f30a97
jang_yoonsu <shagrat8546@gmail.com>
2024-05-25 00:18:15 +0900

    feat : add index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import * as networkErrorHandler from "./network-error-handler";
+
+import type {
+  Handlers,
+  BaseError,
+  RegistryErrorHandler,
+  AddHandlerType,
+} from "./types";
+
+export {
+  networkErrorHandler,
+  Handlers,
+  BaseError,
+  RegistryErrorHandler,
+  AddHandlerType,
+};
